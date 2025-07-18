4e4e3743ea51ce5607b3ea1a97285a1d9462d922
luoluoluoouo <jimmy0209jj@gmail.com>
2025-07-18 15:40:43 +0800

    cmd_dict

--- multi_drone_ctl/drone_ctl.py
+++ multi_drone_ctl/drone_ctl.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
-
+import threading
+import time
 import rclpy
+
+from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 from px4_msgs.msg import OffboardControlMode, TrajectorySetpoint, VehicleCommand, VehicleLocalPosition, VehicleStatus
 
-prefix = '/px4_2'
-target_system = 3
-
 class OffboardControl(Node):
     """Node for controlling a vehicle in offboard mode."""
 
-    def __init__(self) -> None:
-        super().__init__('offboard_control_takeoff_and_land')
+    def __init__(self, prefix: str, target_system: int) -> None:
+        super().__init__(f'offboard_control_drone_{target_system - 1}')
 
-        # Configure QoS profile for publishing and subscribing
+        self.prefix = prefix
+        self.target_system = target_system
+        self.get_logger().warn(f'{prefix} + {target_system}')
+
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
             durability=DurabilityPolicy.TRANSIENT_LOCAL,
@@ -24,90 +27,71 @@ class OffboardControl(Node):
 
         # Create publishers
         self.offboard_control_mode_publisher = self.create_publisher(
-            OffboardControlMode, prefix + '/fmu/in/offboard_control_mode', qos_profile)
+            OffboardControlMode, f'{prefix}/fmu/in/offboard_control_mode', qos_profile)
         self.trajectory_setpoint_publisher = self.create_publisher(
-            TrajectorySetpoint, prefix + '/fmu/in/trajectory_setpoint', qos_profile)
+            TrajectorySetpoint, f'{prefix}/fmu/in/trajectory_setpoint', qos_profile)
         self.vehicle_command_publisher = self.create_publisher(
-            VehicleCommand, prefix + '/fmu/in/vehicle_command', qos_profile)
+            VehicleCommand, f'{prefix}/fmu/in/vehicle_command', qos_profile)
 
         # Create subscribers
         self.vehicle_local_position_subscriber = self.create_subscription(
-            VehicleLocalPosition, prefix + '/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
+            VehicleLocalPosition, f'{prefix}/fmu/out/vehicle_local_position', self.vehicle_local_position_callback, qos_profile)
         self.vehicle_status_subscriber = self.create_subscription(
-            VehicleStatus, prefix + '/fmu/out/vehicle_status_v1', self.vehicle_status_callback, qos_profile)
+            VehicleStatus, f'{prefix}/fmu/out/vehicle_status_v1', self.vehicle_status_callback, qos_profile)
 
-        # Initialize variables
         self.offboard_setpoint_counter = 0
         self.vehicle_local_position = VehicleLocalPosition()
         self.vehicle_status = VehicleStatus()
         self.takeoff_height = -5.0
 
-        # Create a timer to publish control commands
         self.timer = self.create_timer(0.05, self.timer_callback)
 
-    def vehicle_local_position_callback(self, vehicle_local_position):
-        """Callback function for vehicle_local_position topic subscriber."""
-        self.vehicle_local_position = vehicle_local_position
+    def vehicle_local_position_callback(self, msg):
+        self.vehicle_local_position = msg
 
-    def vehicle_status_callback(self, vehicle_status):
-        """Callback function for vehicle_status topic subscriber."""
-        self.vehicle_status = vehicle_status
+    def vehicle_status_callback(self, msg):
+        self.vehicle_status = msg
 
     def arm(self):
-        """Send an arm command to the vehicle."""
-        self.publish_vehicle_command(
-            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=1.0)
         self.get_logger().info('Arm command sent')
 
     def disarm(self):
-        """Send a disarm command to the vehicle."""
-        self.publish_vehicle_command(
-            VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=0.0)
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_COMPONENT_ARM_DISARM, param1=0.0)
         self.get_logger().info('Disarm command sent')
 
     def engage_offboard_mode(self):
-        """Switch to offboard mode."""
-        self.publish_vehicle_command(
-            VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
+        self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_DO_SET_MODE, param1=1.0, param2=6.0)
         self.get_logger().info("Switching to offboard mode")
 
     def land(self):
-        """Switch to land mode."""
         self.publish_vehicle_command(VehicleCommand.VEHICLE_CMD_NAV_LAND)
         self.get_logger().info("Switching to land mode")
 
     def publish_offboard_control_heartbeat_signal(self):
-        """Publish the offboard control mode."""
         msg = OffboardControlMode()
         msg.position = True
-        msg.velocity = False
-        msg.acceleration = False
-        msg.attitude = False
-        msg.body_rate = False
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.offboard_control_mode_publisher.publish(msg)
 
     def publish_position_setpoint(self, x: float, y: float, z: float):
-        """Publish the trajectory setpoint."""
         msg = TrajectorySetpoint()
         msg.position = [x, y, z]
-        msg.yaw = 1.57079  # (90 degree)
+        msg.yaw = 1.57079
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.trajectory_setpoint_publisher.publish(msg)
-        self.get_logger().info(f"Publishing position setpoints {[x, y, z]}")
+        # self.get_logger().info(f"Publishing position setpoints {[x, y, z]}")
 
-    def publish_vehicle_command(self, command, **params) -> None:
-        """Publish a vehicle command."""
+    def publish_vehicle_command(self, command, **params):
         msg = VehicleCommand()
         msg.command = command
         msg.param1 = params.get("param1", 0.0)
         msg.param2 = params.get("param2", 0.0)
-        msg.param3 = params.get("param3", 0.0)
-        msg.param4 = params.get("param4", 0.0)
+        msg.param3 = params.get("param4", 0.0)
         msg.param5 = params.get("param5", 0.0)
         msg.param6 = params.get("param6", 0.0)
         msg.param7 = params.get("param7", 0.0)
-        msg.target_system = target_system
+        msg.target_system = self.target_system
         msg.target_component = 1
         msg.source_system = 1
         msg.source_component = 1
@@ -115,34 +99,85 @@ class OffboardControl(Node):
         msg.timestamp = int(self.get_clock().now().nanoseconds / 1000)
         self.vehicle_command_publisher.publish(msg)
 
-    def timer_callback(self) -> None:
-        """Callback function for the timer."""
+    def timer_callback(self):
         self.publish_offboard_control_heartbeat_signal()
 
         if self.offboard_setpoint_counter == 10:
             self.engage_offboard_mode()
             self.arm()
 
-        # self.get_logger().warn(f"vehicle_status.nav_state {self.vehicle_status.nav_state}")
-        # self.get_logger().warn(f"NAVIGATION_STATE_OFFBOARD {VehicleStatus.NAVIGATION_STATE_OFFBOARD}")
-        if self.vehicle_local_position.z > self.takeoff_height and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
-            self.publish_position_setpoint(0.0, 0.0, self.takeoff_height)
+        # if self.vehicle_local_position.z > self.takeoff_height \
+        #   and self.vehicle_status.nav_state == VehicleStatus.NAVIGATION_STATE_OFFBOARD:
+        #     self.publish_position_setpoint(0.0, 0.0, self.takeoff_height)
+        # elif self.vehicle_local_position.z <= self.takeoff_height:
+        #     self.land()
 
-        elif self.vehicle_local_position.z <= self.takeoff_height:
-            self.land()
-            exit(0)
+        self.x = self.vehicle_local_position.x
+        self.y = self.vehicle_local_position.y
+        self.z = self.vehicle_local_position.z
 
         if self.offboard_setpoint_counter < 11:
             self.offboard_setpoint_counter += 1
+    
+class DroneController():
+    """Controller for managing multiple drones."""
+
+    def __init__(self, drones):
+        self.drones = drones
+
+        executor = MultiThreadedExecutor()
+        for drone in drones:
+            executor.add_node(drone)
+
+        self.executor_thread = threading.Thread(target=self.run_executor, args=(executor,))
+        self.executor_thread.start()
+        
+    def run_executor(self, executor):
+        executor.spin()
+
+    def position_setpoint(self, drone_ID = 0,x: float = 0.0, y: float = 0.0, z: float = -5.0):
+        drone = self.drones[drone_ID]
+        drone.publish_position_setpoint(x, y, z)
+
+    def run_position_setpoint(self, drone_ID=0, x: float = 0.0, y: float = 0.0, z: float = -5.0):
+        self.position_setpoint_thread = threading.Thread(
+            target=self.position_setpoint, args=(drone_ID, x, y, z))
+        self.position_setpoint_thread.start()
 
 
 def main(args=None) -> None:
-    print('Starting offboard control node...')
     rclpy.init(args=args)
-    offboard_control = OffboardControl()
-    rclpy.spin(offboard_control)
-    offboard_control.destroy_node()
-    rclpy.shutdown()
+    ID = [1, 2]
+    drones = []
+    for i in ID:
+        prefix = f'/px4_{i}'
+        target_system = i + 1
+        drone = OffboardControl(prefix, target_system)
+        drones.append(drone)
+
+    controller = DroneController(drones)
+    cmd_dict = {
+        'set': lambda z: controller.position_setpoint(z),
+        'disarm': lambda: [drone.disarm() for drone in drones],
+        'land': lambda: [drone.land() for drone in drones],
+    }
+
+    while rclpy.ok():
+        try:
+            cmd = input("Enter command (set, disarm, land): ").strip().lower()
+            if cmd in cmd_dict:
+                if cmd == 'set':
+                    drone_id = int(input("Enter drone ID (0 or 1): ").strip())
+                    x = float(input("Enter x position: ").strip())
+                    y = float(input("Enter y position: ").strip())
+                    z = float(input("Enter z position: ").strip())
+                    controller.run_position_setpoint(drone_id, x, y, z)
+                else:
+                    cmd_dict[cmd]()
+            else:
+                print("Unknown command. Please enter 'setpoint', 'disarm', or 'land'.")
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == '__main__':
