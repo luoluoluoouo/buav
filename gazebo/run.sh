a5b77e44d27cf50a1706315b28a61b0ff36168bc
umeow0716 <jj30462281@gmail.com>
2025-08-14 16:47:03 +0800

    kill gagagazebo

--- gazebo/run.sh
+++ gazebo/run.sh
@@ -1,6 +1,11 @@
-!#/bin/bash
+#!/bin/sh
+
+kill -9 $(lsof -t -i :11345)
+
+# 取得腳本所在資料夾的絕對路徑
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Setting AutoPilot path
-export PX4_AUTOPILOT_PATH=/home/ada/luoluo/PX4-Autopilot
+export PX4_AUTOPILOT_PATH=/home/ubuntu/PX4-Autopilot
 
-$PX4_AUTOPILOT_PATH/Tools/simulation/gazebo-classic/sitl_multiple_run.sh -m iris -n 2 -w ../../../../../../../../../../../../../../../../../../../../../../../$(pwd)/ble
+$PX4_AUTOPILOT_PATH/Tools/simulation/gazebo-classic/sitl_multiple_run.sh -m iris -n 2 -w ../../../../../../../../../../../../../../../../../../../../../../../$SCRIPT_DIR/ble
