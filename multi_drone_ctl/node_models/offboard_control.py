82536a99c5dbb98a25a12f0c54e5dbd59f83035a
umeow0716 <jj30462281@gmail.com>
2025-08-16 21:03:06 +0800

    check arm status and lock position

--- multi_drone_ctl/node_models/offboard_control.py
+++ multi_drone_ctl/node_models/offboard_control.py
@@ -1,12 +1,14 @@
 from rclpy.node import Node
 from rclpy.qos import QoSProfile
 
-from ..publisher.offboard_control_mode_publisher import OffboardControlModePublisher
+
 from ..publisher.trajectory_setpoint_publisher import TrajectorySetpointPublisher
+from ..publisher.offboard_control_mode_publisher import OffboardControlModePublisher
 from ..publisher.vehicle_command_publisher import VehicleCommandPublisher, VehicleCommandEnum
 
+from ..receiver.vehicle_status_receiver import VehicleStatusEnum, VehicleStatusReceiver, VehicleStatusMsg
 from ..receiver.vehicle_local_position_receiver import VehicleLocalPositionReceiver, VehicleLocalPositionMsg
-from ..receiver.vehicle_status_receiver import VehicleStatusReceiver, VehicleStatusMsg
+from ..receiver.vehicle_command_ack import VehicleCommandAckReceiver, VehicleCommandAckMsg
 
 class OffboardControl(Node):
     """Node for controlling a vehicle in offboard mode."""
@@ -22,122 +24,131 @@ class OffboardControl(Node):
         self.y = self.gazebo_y
         self.z = self.gazebo_z
         
+        self._target_x = 0.0
+        self._target_y = 0.0
+        self._target_z = 0.0
+        self._target_yaw = 0.0
+        
         self._offboard_control_mode_publisher = OffboardControlModePublisher(self, qos_profile, prefix)
         self._trajectory_setpoint_publisher = TrajectorySetpointPublisher(self, qos_profile, prefix)
         self._vehicle_command_publisher = VehicleCommandPublisher(self, qos_profile, prefix)
 
         self._vehicle_local_position_receiver = VehicleLocalPositionReceiver(self, qos_profile, prefix)
-        self._vehicle_local_position_receiver.add_callback(self.vehicle_local_position_callback)
+        self._vehicle_local_position_receiver.add_callback(self._vehicle_local_position_callback)
         
         self._vehicle_status_receiver = VehicleStatusReceiver(self, qos_profile, prefix)
-        self._vehicle_status_receiver.add_callback(self.vehicle_status_callback)
+        self._vehicle_status_receiver.add_callback(self._vehicle_status_callback)
 
         self.offboard_setpoint_counter = 0
+        self.vehicle_local_position = self._vehicle_local_position_receiver.get_simple_msg()
         self.vehicle_status = self._vehicle_status_receiver.get_simple_msg()
-        self.takeoff_height = -5.0
 
-        self.timer = self.create_timer(0.05, self._timer_callback)
+        self.timer = self.create_timer(0.1, self._timer_callback)
+        
+        self.flying = False
         
         self.get_logger().warn(f'{prefix} + {target_system}')
 
-    def vehicle_local_position_callback(self, msg: VehicleLocalPositionMsg):
-        if msg is not None:
-            self.vehicle_local_position = msg
-            self.x = self.vehicle_local_position.x
-            self.y = self.vehicle_local_position.y
-            self.z = self.vehicle_local_position.z
-        else:
-            self.get_logger().warn(f'No data received from {self.prefix}/fmu/out/vehicle_local_position')
-
-
-    def vehicle_status_callback(self, msg: VehicleStatusMsg):
-        self.vehicle_status = msg
-
-    def arm(self):
+    def arm(self) -> None:
         self._vehicle_command_publisher.publish(
             command_id=VehicleCommandEnum.VEHICLE_CMD_COMPONENT_ARM_DISARM,
             param1=VehicleCommandEnum.ARMING_ACTION_ARM,
             target_system=self.target_system,
-            target_component=1,
-            source_system=1,
-            source_component=1,
-            from_external=True
         )
 
-    def disarm(self):
+    def disarm(self) -> None:
         self._vehicle_command_publisher.publish(
             command_id=VehicleCommandEnum.VEHICLE_CMD_COMPONENT_ARM_DISARM,
             param1=VehicleCommandEnum.ARMING_ACTION_DISARM,
             target_system=self.target_system,
-            target_component=1,
-            source_system=1,
-            source_component=1,
-            from_external=True
         )
 
-    def engage_offboard_mode(self):
+    def engage_offboard_mode(self) -> None:
         self._vehicle_command_publisher.publish(
             command_id=VehicleCommandEnum.VEHICLE_CMD_DO_SET_MODE,
             param1=1.0,
             param2=6.0,
             target_system=self.target_system,
-            target_component=1,
-            source_system=1,
-            source_component=1,
-            from_external=True
         )
 
-    def land(self):
+    def land(self) -> None:
         self._vehicle_command_publisher.publish(
             command_id=VehicleCommandEnum.VEHICLE_CMD_NAV_LAND,
             target_system=self.target_system,
-            target_component=1,
-            source_system=1,
-            source_component=1,
-            from_external=True
         )
-
-    def set_position(self, target_position: tuple[float, float, float]):
-        target_x, target_y, target_z = target_position
-        x = target_y - self.gazebo_y
-        y = target_x - self.gazebo_x
-        z = self.gazebo_z - target_z
         
-        self._trajectory_setpoint_publisher.publish(
-            position=(x, y, z),
-            yaw=0.0,
-        )
+        self.flying = False
 
-    def get_position(self):
+    def set_position(self, target_x: float, target_y: float, target_z: float) -> None:
+        if target_z < 2.0:
+            raise ValueError("Target Z position must be at least 2.0 meters above the ground.")
+            
+        self._target_x = target_y - self.gazebo_y
+        self._target_y = target_x - self.gazebo_x
+        self._target_z = self.gazebo_z - target_z
+        
+        self.flying = True
+
+    def get_position(self) -> tuple[float, float, float]:
         self.abs_x = self.gazebo_x + self.vehicle_local_position.y
         self.abs_y = self.gazebo_y + self.vehicle_local_position.x
         self.abs_z = self.gazebo_z - self.vehicle_local_position.z
         return (self.abs_x, self.abs_y, self.abs_z)
-    
-    def heartbeat(self):
-        self._offboard_control_mode_publisher.publish(position=True)
-    
-    def _timer_callback(self):
-        self.heartbeat()
 
-        if self.offboard_setpoint_counter == 10:
+    def is_armed(self) -> bool:
+        return self.vehicle_status.arming_state == VehicleStatusEnum.ARMING_STATE_ARMED
+    
+    def _vehicle_local_position_callback(self, msg: VehicleLocalPositionMsg) -> None:
+        self.vehicle_local_position = msg
+        self.x = self.vehicle_local_position.x
+        self.y = self.vehicle_local_position.y
+        self.z = self.vehicle_local_position.z
+        
+        if self.flying:
+            print(msg.x, msg.y, msg.z)
+
+    def _vehicle_status_callback(self, msg: VehicleStatusMsg) -> None:
+        self.vehicle_status = msg
+    
+    def _vehicle_command_ack_callback(self, msg: VehicleCommandAckMsg) -> None:
+        pass
+    
+    def _timer_callback(self) -> None:
+        """Callback function for the timer."""
+        self._offboard_control_mode_publisher.heartbeat()
+        
+        if self.offboard_setpoint_counter == 15:
             self.engage_offboard_mode()
-            self.arm()
-
-        if self.offboard_setpoint_counter < 11:
+            
+        if self.flying:
+            self._keep_status()
+            self._trajectory_setpoint_publisher.publish(
+                position=(self._target_x, self._target_y, self._target_z),
+                yaw=self._target_yaw
+            )
+            
+        if self.offboard_setpoint_counter < 16:
             self.offboard_setpoint_counter += 1
     
-    def get_offboard_control_mode_publisher(self):
+    def _keep_status(self) -> None:
+        """Ensure the vehicle is in offboard mode and armed."""
+        if self.vehicle_status.nav_state != VehicleStatusEnum.NAVIGATION_STATE_OFFBOARD:
+            self.engage_offboard_mode()
+        
+        if not self.is_armed():
+            self.arm()
+
+    def get_offboard_control_mode_publisher(self) -> OffboardControlModePublisher:
         return self._offboard_control_mode_publisher
     
-    def get_trajectory_setpoint_publisher(self):
+    def get_trajectory_setpoint_publisher(self) -> TrajectorySetpointPublisher:
         return self._trajectory_setpoint_publisher
     
-    def get_vehicle_command_publisher(self):
+    def get_vehicle_command_publisher(self) -> VehicleCommandPublisher:
         return self._vehicle_command_publisher
     
-    def get_vehicle_local_position_receiver(self):
+    def get_vehicle_local_position_receiver(self) -> VehicleLocalPositionReceiver:
         return self._vehicle_local_position_receiver
     
-    def get_vehicle_status_receiver(self):
+    def get_vehicle_status_receiver(self) -> VehicleStatusReceiver:
         return self._vehicle_status_receiver
