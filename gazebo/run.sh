a5b79362e30b7311466323c8a53feb9828a04ba8
umeow0716 <jj30462281@gmail.com>
2025-08-16 21:14:26 +0800

    把 run.sh 變漂亮的魔法

--- gazebo/run.sh
+++ gazebo/run.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 
+get_root_back() {
+    path="$1"
+    # 去掉開頭的 "/" 避免第一個空元素
+    clean=$(echo "$path" | sed 's:^/::')
+    # 用 "/" 切開計算有幾層
+    depth=$(echo "$clean" | awk -F/ '{print NF}')
+
+    out="/"
+    i=0
+    while [ "$i" -lt "$depth" ]; do
+        out="../$out"
+        i=$((i + 1))
+    done
+
+    echo "$out"
+}
+
 kill -9 $(lsof -t -i :11345)
 kill -9 $(lsof -t -i :8888)
 
@@ -13,7 +30,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Setting AutoPilot path
 export PX4_AUTOPILOT_PATH=/home/ubuntu/PX4-Autopilot
 
-$PX4_AUTOPILOT_PATH/Tools/simulation/gazebo-classic/sitl_multiple_run.sh -m iris -n 2 -w ../../../../../../../../../../../../../../../../../../../../../../../$SCRIPT_DIR/ble
+$PX4_AUTOPILOT_PATH/Tools/simulation/gazebo-classic/sitl_multiple_run.sh -m iris -n 2 -w $(get_root_back "$PX4_AUTOPILOT_PATH/Tools/simulation/gazebo-classic/sitl_gazebo-classic/worlds")$SCRIPT_DIR/ble -s iris:2 -t px4_sitl_default
 
 kill -9 $(lsof -t -i :11345)
 kill -9 $(lsof -t -i :8888)
