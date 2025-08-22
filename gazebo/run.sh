91a797dd7a93d7b2a1a5311cf93cf3c85f492271
luoluoluoouo <jimmy0209jj@gmail.com>
2025-08-22 17:17:06 +0800

    可以開模擬但不能自訂world

--- gazebo/run.sh
+++ gazebo/run.sh
@@ -36,21 +36,26 @@ trap cleanup INT TERM
 kill -9 $(lsof -t -i :11345) 2>/dev/null
 kill -9 $(lsof -t -i :8888) 2>/dev/null
 
-MicroXRCEAgent udp4 -p 8888 &
-
-kill -9 $(lsof -t -i :11345) 2>/dev/null
-
 # 取得腳本所在資料夾的絕對路徑
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+touch $SCRIPT_DIR/logs/microxrceagent.log
+MicroXRCEAgent udp4 -p 8888 >$SCRIPT_DIR/logs/microxrceagent.log 2>&1 &
 
 # Setting AutoPilot path
 export PX4_AUTOPILOT_PATH=/home/ada/luoluo/PX4-Autopilot
 
 python3 send_heartbeat.py &
 
-# Open in new terminals
-PX4_SYS_AUTOSTART=4001 PX4_GZ_MODEL_POSE="0,0" PX4_SIM_MODEL=gz_x500 $PX4_AUTOPILOT_PATH/build/px4_sitl_default/bin/px4 -i 1 &
-PX4_SYS_AUTOSTART=4001 PX4_GZ_MODEL_POSE="0,1" PX4_SIM_MODEL=gz_x500 PX4_GZ_STANDALONE=1 $PX4_AUTOPILOT_PATH/build/px4_sitl_default/bin/px4 -i 2 &
+sleep 3
+
+PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 PX4_GZ_MODEL_POSE="0,3" \
+$PX4_AUTOPILOT_PATH/build/px4_sitl_default/bin/px4 -i 1 &
+PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 PX4_GZ_MODEL_POSE="0,-3" \
+$PX4_AUTOPILOT_PATH/build/px4_sitl_default/bin/px4 -i 2 &
+
+# PX4_GZ_STANDALONE=1 PX4_SYS_AUTOSTART=4001 PX4_SIM_MODEL=gz_x500 PX4_GZ_MODEL_POSE="0,0" \
+# $PX4_AUTOPILOT_PATH/build/px4_sitl_default/bin/px4 -i 0 &
 
 # 等待信號
 echo "按 Ctrl+C 停止..."
