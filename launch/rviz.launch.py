13942d30f8c83f4eecc51ad2d2cc28c08cf2e003
luoluoluoouo <jimmy0209jj@gmail.com>
2025-09-03 15:47:17 +0800

    gz_gridge 加上 dorne_name

--- launch/rviz.launch.py
+++ launch/rviz.launch.py
@@ -7,24 +7,25 @@ def generate_launch_description():
     pkg_share = FindPackageShare('buav').find('buav')
     rviz_config = os.path.join(pkg_share, 'rviz', 'point_image.rviz')
     drone_id = "x500_depth_0"
+    world_name = "default"
 
     return LaunchDescription([
         # RGB Image
         Node(
             package="ros_gz_bridge",
             executable="parameter_bridge",
-            arguments=[f"/world/walls/model/{drone_id}/link/camera_link/sensor/IMX214/image"
+            arguments=[f"/world/{world_name}/model/{drone_id}/link/camera_link/sensor/IMX214/image"
                        "@sensor_msgs/msg/Image@gz.msgs.Image"],
-            remappings=[(f"/world/walls/model/{drone_id}/link/camera_link/sensor/IMX214/image",
+            remappings=[(f"/world/{world_name}/model/{drone_id}/link/camera_link/sensor/IMX214/image",
                          "/camera/image")]
         ),
         # CameraInfo
         Node(
             package="ros_gz_bridge",
             executable="parameter_bridge",
-            arguments=[f"/world/walls/model/{drone_id}/link/camera_link/sensor/IMX214/camera_info"
+            arguments=[f"/world/{world_name}/model/{drone_id}/link/camera_link/sensor/IMX214/camera_info"
                        "@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo"],
-            remappings=[(f"/world/walls/model/{drone_id}/link/camera_link/sensor/IMX214/camera_info",
+            remappings=[(f"/world/{world_name}/model/{drone_id}/link/camera_link/sensor/IMX214/camera_info",
                          "/camera/camera_info")]
         ),
         # PointCloud
